cbfd9c0dcbe70b3209741890dc2c03b8e3fd5ace
zulisk <zulfikar.iskandar@kana.co.id>
2015-11-12 16:48:32 +0700

    update copytext header

--- html/header.php
+++ html/header.php
@@ -12,13 +12,11 @@
 					<img align="right" src="assets/images/goodyear-onecard.png" alt="goodyear onecard" class="goodyearonecard wow slideInUp" data-wow-duration="1s">
                		<h1>#one<span class="yellow">good</span>card</h1>
                		<p class="yellow wow slideInUp" data-wow-duration="1s">Nikmati kemudahan dan kepraktisan kartu Goodyear Mandiri One Card. 
-Sudah tidak zamannya lagi mengantri bayar tol, tinggal tempel kartu 
-dan jalan.Untuk kamu yang belum memiliki kartu spesial ini, 
-Goodyear akan memberikannya secara gratis untukmu.</p>
+					Sudah tidak zamannya lagi mengantri bayar tol, tinggal tempel kartu dan jalan.Untuk Anda yang belum memiliki kartu spesial ini, 
+					Goodyear akan memberikannya secara gratis untuk Anda!</p>
 
-<p class="yellow wow slideInUp" data-wow-duration="1s">Setiap kartu Goodyear Mandiri One Card berisi saldo 
-senilai Rp50.000,-.Tunggu apalagi? Dapatkan kartu 
-Goodyear Mandiri One Card kamu sekarang juga!</p>
+<p class="yellow wow slideInUp" data-wow-duration="1s">Setiap kartu Goodyear Mandiri One Card berisi saldo senilai Rp50.000,-.
+Tunggu apalagi? Miliki kartu Goodyear Mandiri One Card Anda sekarang juga!</p>
 					<a href="#step" class="scrolldown wow fadeIn" data-wow-duration="0.5s" data-wow-delay="1s"></a>
                </div>
             </div><!-- end .col-md-12 -->
